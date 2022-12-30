8d015e3b8958b0075bdec3912e16e9bd35fc3036
niladri <niladriparamanik@gmail.com>
2022-12-30 18:31:09 +0530

    initail commit 3

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
       BankAccount b=new BankAccount("abc",1000,500);
       try{
-          System.out.print(b.generateAccountNumber(3,20));
+          System.out.print(b.generateAccountNumber(3,0));
       }catch (Exception e){
           System.out.print("Error");
       }
